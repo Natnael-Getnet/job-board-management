38285eca0dac0f80f56b5a6fa9b0d723edd24dac
Natnael-Getnet <natnaelgetnetharegewoyn@gmail.com>
2024-11-19 12:33:13 +0300

    Addded the hero section

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue'
+import Hero from './components/Hero.vue'
 </script>
 
-<template><NavBar /></template>
+<template><NavBar /> <Hero /></template>
