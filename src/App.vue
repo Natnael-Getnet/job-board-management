62712a3260fff5fd31b418f868b6bd5d592511e8
Natnael-Getnet <natnaelgetnetharegewoyn@gmail.com>
2024-11-19 13:09:12 +0300

    Added Card section

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue'
 import Hero from './components/Hero.vue'
+import HomeCard from './components/HomeCard.vue'
 </script>
 
-<template><NavBar /> <Hero /></template>
+<template>
+  <NavBar />
+  <Hero />
+  <HomeCard />
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps({
+  bgColor: { type: String, default: 'bg-gray-100' },
+})
+</script>
+
+<template>
+  <div :class="`${bgColor} p-6 rounded-lg shadow-md`">
+    <slot></slot>
+  </div>
+</template>
