0805fba4ff48d4f1dad8cd1ad996e923c446b432
Natnael-Getnet <natnaelgetnetharegewoyn@gmail.com>
2024-11-19 17:24:37 +0300

    Refactor: Added a limit and showButton to the jobListings

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,11 @@ import NavBar from "@/components/NavBar.vue";
 import Hero from "@/components/Hero.vue";
 import HomeCard from "@/components/HomeCard.vue";
 import JobListings from "@/components/JobListings.vue";
-import AllJobs from "@/components/AllJobs.vue";
 </script>
 
 <template>
   <NavBar />
   <Hero />
   <HomeCard />
-  <JobListings />
-  <AllJobs />
+  <JobListings :limit="3" :showButton="true" />
 </template>
