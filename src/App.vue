0b4935742949865da78a1a60c3d112c92680dffc
Natnael-Getnet <natnaelgetnetharegewoyn@gmail.com>
2024-11-21 18:07:39 +0300

    Setup router and added home route

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
 import NavBar from "@/components/NavBar.vue";
-import Hero from "@/components/Hero.vue";
-import HomeCard from "@/components/HomeCard.vue";
-import JobListings from "@/components/JobListings.vue";
 </script>
 
 <template>
   <NavBar />
-  <Hero />
-  <HomeCard />
-  <JobListings :limit="3" :showButton="true" />
+  <RouterView />
 </template>
