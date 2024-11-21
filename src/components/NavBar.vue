a43948639f3c4338447d29dbe62bd7c67634a5ce
Natnael-Getnet <natnaelgetnetharegewoyn@gmail.com>
2024-11-21 23:37:27 +0300

    Refactor: Highlight the current path in the navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import logo from "@/assets/img/logo.png";
+import { useRoute } from "vue-router";
+
+const isActiveLink = (route: string) => {
+  const currentRoute = useRoute();
+  return route === currentRoute.path;
+};
 </script>
 
 <template>
@@ -16,17 +22,17 @@ import logo from "@/assets/img/logo.png";
             <div class="flex space-x-2">
               <RouterLink
                 to="/"
-                class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                :class="`text-white ${isActiveLink('/') ? 'bg-green-900' : ''}  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`"
                 >Home</RouterLink
               >
               <RouterLink
                 to="/jobs"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                :class="`text-white ${isActiveLink('/jobs') ? 'bg-green-900' : ''}  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`"
                 >Jobs</RouterLink
               >
               <RouterLink
                 to="/jobs/add"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                :class="`text-white ${isActiveLink('/jobs/add') ? 'bg-green-900' : ''}  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`"
                 >Add Job</RouterLink
               >
             </div>
