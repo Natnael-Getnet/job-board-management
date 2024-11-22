beac60568a1dc8dc1163f895091f65f624cf48a9
Natnael-Getnet <natnaelgetnetharegewoyn@gmail.com>
2024-11-22 11:34:54 +0300

    Added a job detail view

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createMemoryHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
+import JobView from "@/views/JobView.vue";
 
 const router = createRouter({
   history: createMemoryHistory(import.meta.env.BASE_URL),
@@ -11,10 +12,16 @@ const router = createRouter({
       name: "home",
       component: HomeView,
     },
+
     {
       path: "/jobs",
       name: "jobs",
       component: JobsView,
+    },
+    {
+      path: "/jobs/:id",
+      name: "job",
+      component: JobView,
     },
     {
       path: "/:catchAll(.*)",
